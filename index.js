fe874f88af80b144c4815b5cdb709face36b68fb
Hoopsy <renanhoruz@gmail.com>
2025-11-01 10:38:36 +0200

    Navbar and body set up, next is content of bodies

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function Goober() {
   document.getElementById('goober').innerHTML = 'Bye'
 }
+function Goto(name) {
+  switch (name) {
+    case "home":
+      window.location.href = "./index.html";
+      break;
+    case "prod":
+      window.location.href = "./products.html";
+      break;
+    case "contact":
+      window.location.href = "./contacts.html";
+      break;
+  }
+}
